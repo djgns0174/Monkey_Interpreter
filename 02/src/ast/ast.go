6bd1f43830e11dc5ec1fe4444963dc905192aec0
djgns0174 <djgns0174@naver.com>
2025-01-30 01:34:43 +0900

    2. 파싱 함수리터럴 노드 정의

--- 02/src/ast/ast.go
+++ 02/src/ast/ast.go
@@ -3,6 +3,7 @@ package ast
 import (
 	"bytes"
 	"monkey/token"
+	"strings"
 )
 
 type Node interface {
@@ -180,6 +181,31 @@ func (ie *IfExpression) String() string {
 	return out.String()
 }
 
+type FunctionLiteral struct {
+	Token      token.Token
+	Parameters []Identifier
+	Body       *BlockStatement
+}
+
+func (fl *FunctionLiteral) expressionNode()      {}
+func (fl *FunctionLiteral) TokenLiteral() string { return fl.Token.Literal }
+func (fl *FunctionLiteral) String() string {
+	var out bytes.Buffer
+
+	params := []string{}
+	for _, p := range fl.Parameters {
+		params = append(params, p.String())
+	}
+
+	out.WriteString(fl.TokenLiteral())
+	out.WriteString("(")
+	out.WriteString(strings.Join(params, ", "))
+	out.WriteString(")")
+	out.WriteString(fl.Body.String()) // {} 이 어떻게 파싱이 되면 중괄호를 추가하지 않아도 되는지 파악.
+
+	return out.String()
+}
+
 type BlockStatement struct {
 	Token      token.Token
 	Statements []Statement
@@ -190,9 +216,13 @@ func (bs *BlockStatement) TokenLiteral() string { return bs.Token.Literal }
 func (bs *BlockStatement) String() string {
 	var out bytes.Buffer
 
+	out.WriteString("{")
+
 	for _, s := range bs.Statements {
 		out.WriteString(s.String())
 	}
+
+	out.WriteString("}")
 
 	return out.String()
 }
